028f7b0a6af86f1744aeeb04acd9b8303cd1c751
Mina Hany <10minahany10@gmail.com>
2023-10-21 15:46:11 +0300

    Second and Third Solution

--- PaymobTask.playground/Contents.swift
+++ PaymobTask.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 var greeting = "Hello, PayMob"
 //===============================================/
 //MARK: - First -
-var arr = [2,4,6,1,3,7,9,8,10]
+var arr = [1,2,4]
 //[2,3,1,5] //[1,2,4] //[2,4,6,1,3,7,9,8,10]
 print(solution(&arr))
 
@@ -20,4 +20,61 @@ public func solution(_ A: inout [Int]) -> Int {
     print(sum)
     let missingElem = totalArraySum - sum
     return missingElem
+}
+//===============================================/
+//MARK: - Second -
+
+var arr1 = [3,4,4,6,1,4,4]
+solution1(5, A: &arr1)
+/// increase(X) − counter X is increased by 1,
+/// max counter − all counters are set to the maximum value of any counter.
+/// if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+/// if A[K] = N + 1 then operation K is max counter.
+///
+public func solution1(_ N: Int, A: inout [Int]) {
+    var counters = Array(repeating: 0, count: N)
+    var max = 0
+    let cond = N + 1
+    var position = 0
+    for index in 0..<A.count {
+        if A[index] == cond {
+            counters = Array(repeating: max, count: N)
+        } else {
+            position = A[index] - 1
+            counters[position] = counters[position] + 1
+            if counters[position] > max {
+                max = counters[position]
+            }
+        }
+       print(counters)
+    }
+    
+}
+
+//===============================================/
+//MARK: - Third -
+var arr2 = [3,4,4,6,1,4,4]
+print("Our Array",solution2(5, A: &arr1))
+/// increase(X) − counter X is increased by 1,
+/// max counter − all counters are set to the maximum value of any counter.
+/// if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+/// if A[K] = N + 1 then operation K is max counter.
+public func solution2(_ N: Int, A: inout [Int]) -> [Int] {
+    var counters = Array(repeating: 0, count: N)
+    var max = 0
+    let cond = N + 1
+    var position = 0
+    for index in 0..<A.count {
+        if A[index] == cond {
+            counters = Array(repeating: max, count: N)
+        } else {
+            position = A[index] - 1
+            counters[position] = counters[position] + 1
+            if counters[position] > max {
+                max = counters[position]
+            }
+        }
+       print(counters)
+    }
+    return counters
 }
